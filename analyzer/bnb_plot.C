a7514ec284bcc031d6e0d491bf5df43a967ca0ac
Wooyoung Jang <wooyoung.jang@uta.edu>
2026-02-19 12:33:14 -0600

    analysis variables renamed

--- analyzer/bnb_plot.C
+++ analyzer/bnb_plot.C
@@ -17,27 +17,20 @@ void bnb_plot(std::string inputFile="input.root", std::string outputFile="output
     auto df_nue = df_valid.Filter("daughterPDG == 12 && daughterPz > 0");
     auto df_nuebar = df_valid.Filter("daughterPDG == -12 && daughterPz > 0");
 
-    /*
-    auto df_numu_activeVol = df_valid.Filter("daughterPDG=14 && daughterPz > 0 && x_at_110m > -1.26 && x_at_110m < 2.74 && y_at_110m > -2.00 && y_at_110m < 2.00");
-    auto df_numubar_activeVol = df_valid.Filter("daughterPDG=-14 && daughterPz > 0 && x_at_110m > -1.26 && x_at_110m < 2.74 && y_at_110m > -2.00 && y_at_110m < 2.00");
-    auto df_numu_activeVol = df_valid.Filter("daughterPDG=12 && daughterPz > 0 && x_at_110m > -1.26 && x_at_110m < 2.74 && y_at_110m > -2.00 && y_at_110m < 2.00");
-    auto df_numubar_activeVol = df_valid.Filter("daughterPDG=-12 && daughterPz > 0 && x_at_110m > -1.26 && x_at_110m < 2.74 && y_at_110m > -2.00 && y_at_110m < 2.00");
-    */
-
     auto h_energy = df_valid.Histo1D(
         {"h_energy", "Neutrino Energy; Energy (GeV);Counts", 100, 0, 5},
         "daughterE"
     );
-
     auto h_profile = df_valid.Histo2D(
         {"h_profile", "Neutrino Profile at SBND(110 m); x []; y [m]", 100, -5, 5, 100, -5, 5},
-        "x_sbnd", "y_at_110m"
+        "x_ff_sbndCoord", "y_ff_beamCoord"
     );
 
-    auto df_numu_ff = df_numu.Filter("x_at_110m > -1.26 && x_at_110m < 2.74 && y_at_110m > -2.00 && y_at_110m < 2.00");
-    auto df_numubar_ff = df_numubar.Filter("x_at_110m > -1.26 && x_at_110m < 2.74 && y_at_110m > -2.00 && y_at_110m < 2.00");
-    auto df_nue_ff = df_nue.Filter("x_at_110m > -1.26 && x_at_110m < 2.74 && y_at_110m > -2.00 && y_at_110m < 2.00");
-    auto df_nuebar_ff = df_nuebar.Filter("x_at_110m > -1.26 && x_at_110m < 2.74 && y_at_110m > -2.00 && y_at_110m < 2.00");
+    // x_ff_sbndCoord = x_ff_beamCoord + 0.74
+    auto df_numu_ff = df_numu.Filter("x_ff_sbndCoord > -2.00 && x_ff_sbndCoord < 2.00 && y_ff_beamCoord > -2.00 && y_ff_beamCoord < 2.00");
+    auto df_numubar_ff = df_numubar.Filter("x_ff_sbndCoord > -2.00 && x_ff_sbndCoord < 2.00 && y_ff_beamCoord > -2.00 && y_ff_beamCoord < 2.00");
+    auto df_nue_ff = df_nue.Filter("x_ff_sbndCoord > -2.00 && x_ff_sbndCoord < 2.00 && y_ff_beamCoord > -2.00 && y_ff_beamCoord < 2.00");
+    auto df_nuebar_ff = df_nuebar.Filter("x_ff_sbndCoord > -2.00 && x_ff_sbndCoord < 2.00 && y_ff_beamCoord > -2.00 && y_ff_beamCoord < 2.00");
 
     auto h_numu_ff_daughterE = df_numu_ff.Histo1D({"h_numu_ff_daughterE", "Numu daughterE after FF; daughterE; Events", 200, 0, 20},"daughterE");
     auto h_numubar_ff_daughterE = df_numubar_ff.Histo1D({"h_numubar_ff_daughterE", "Numubar daughterE after FF; daughterE; Events", 200, 0, 20},"daughterE");
